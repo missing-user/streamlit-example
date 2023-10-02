ec92d33f3d70ae66509c1d775c6f8ef08abd834e
RiaRosenauer <ria@rosenauer-layer.de>
2023-10-02 23:48:56 +0200

    test

--- pages/ria_test.py
+++ pages/ria_test.py
@@ -3,11 +3,7 @@ import pandas as pd
 import datetime
 
 # Sample data
-df = pd.DataFrame(
-    [
-       {"Date": "10.02.2001", "Your Comment": "test", "Comment Friend 1": "test 2"},
-   ]
-)
+df = pd.DataFrame()
 
 # Display the dataframe at the top
 st.table(df)
@@ -20,10 +16,9 @@ st.write(f"Date: {current_date.strftime('%d.%m.%Y')}")
 
 # Input fields for comments
 your_comment = st.text_area("Your Comment", max_chars=1000, height=150)
-friend_comment = st.text_area("Comment Friend 1", max_chars=1000, height=150)
 
 # Button to add the entry to the dataframe
 if st.button("Add Entry"):
-    new_entry = {"Date": current_date.strftime("%d.%m.%Y"), "Your Comment": your_comment, "Comment Friend 1": friend_comment}
+    new_entry = {"Date": current_date.strftime("%d.%m.%Y"), "Your Comment": your_comment}
     df = df.append(new_entry, ignore_index=True)
     st.table(df)  # Update the displayed table with the new entry
