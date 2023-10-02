000f94b2a8066d87933ebc7d36c61de6736e0549
RiaRosenauer <ria@rosenauer-layer.de>
2023-10-02 23:47:44 +0200

    test

--- pages/ria_test.py
+++ pages/ria_test.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import datetime
 
 # Sample data
 df = pd.DataFrame(
@@ -8,16 +9,21 @@ df = pd.DataFrame(
    ]
 )
 
-# Create a sidebar for inputs
-with st.sidebar:
-    date = st.date_input("Date")
-    your_comment = st.text_area("Your Comment", max_chars=1000)
-    friend_comment = st.text_area("Comment Friend 1", max_chars=1000)
-
-    # Button to add the entry to the dataframe
-    if st.button("Add Entry"):
-        new_entry = {"Date": date.strftime("%d.%m.%Y"), "Your Comment": your_comment, "Comment Friend 1": friend_comment}
-        df = df.append(new_entry, ignore_index=True)
-
-# Display the dataframe
+# Display the dataframe at the top
 st.table(df)
+
+# Get the current date
+current_date = datetime.date.today()
+
+# Display the current date without allowing user input (making it a static display)
+st.write(f"Date: {current_date.strftime('%d.%m.%Y')}")
+
+# Input fields for comments
+your_comment = st.text_area("Your Comment", max_chars=1000, height=150)
+friend_comment = st.text_area("Comment Friend 1", max_chars=1000, height=150)
+
+# Button to add the entry to the dataframe
+if st.button("Add Entry"):
+    new_entry = {"Date": current_date.strftime("%d.%m.%Y"), "Your Comment": your_comment, "Comment Friend 1": friend_comment}
+    df = df.append(new_entry, ignore_index=True)
+    st.table(df)  # Update the displayed table with the new entry
