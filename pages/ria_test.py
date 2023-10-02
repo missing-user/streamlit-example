7a9e5577dfc7303e9f2d4fef3ec1b5fc8ec7a66a
RiaRosenauer <ria@rosenauer-layer.de>
2023-10-02 23:43:11 +0200

    test

--- pages/ria_test.py
+++ pages/ria_test.py
@@ -6,7 +6,6 @@ df = pd.DataFrame(
        {"Date": "10.02.2001", "Your Comment": "test", "Comment Friend 1": "test 2"},
    ]
 )
-edited_df = st.data_editor(df)
 
-favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
-st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+
+edited_df = st.data_editor(df)
